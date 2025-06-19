e5c65e1c6c7c0d1b65c000dac28d7b30a7d368df
NyannKunn <160567330+NyannKunn@users.noreply.github.com>
2025-06-19 22:24:45 +0900

    feat: 게임추가기능 구현중, 로그인 버튼(버튼만) 추가
    게임 추가를 모달 창으로 구현중이며
    로그인 버튼은 현재 토글형으로 이부분도 모달로 진행될 예정

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import './Modal.css';
+
+function Modal({ onClose, children }) {
+  return (
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close" onClick={onClose}>✖</button>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
