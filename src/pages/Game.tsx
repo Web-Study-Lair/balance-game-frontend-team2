2ad8a3cf6866fc36652d9cebd2954b386d1c35e5
NyannKunn <160567330+NyannKunn@users.noreply.github.com>
2025-08-08 20:04:21 +0900

    refactor: 서버에서 받아오는 코드, 개인환경에서 사용하는 코드 나눔

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -17,19 +17,6 @@ function Game() {
   const [game, setGame] = useState<GameData | null>(null);
   const [choice, setChoice] = useState('');
 
-
-  // const question = {
-  //   title: ' ',
-  //   option1: {
-  //     text: '평생 치킨만 먹기',
-  //     img: '/images/chicken.jpg', // public 폴더 내 이미지 경로
-  //   },
-  //   option2: {
-  //     text: '평생 라면만 먹기',
-  //     img: '/images/ramen.jpg',
-  //   },
-  // };
-
   useEffect(() => {
     const fetchGame = async () => {
       try {
@@ -80,3 +67,63 @@ function Game() {
 }
 
 export default Game;
+
+
+// import { useState } from 'react';
+// import '../styles/Game.css'; // 스타일 분리 추천
+
+// function Game() {
+
+//   const [choice, setChoice] = useState('');
+
+//   const question = {
+//     title: ' ',
+//     option1: {
+//       text: '평생 치킨만 먹기',
+//       img: '/images/chicken.jpg', // public 폴더 내 이미지 경로
+//     },
+//     option2: {
+//       text: '평생 라면만 먹기',
+//       img: '/images/ramen.jpg',
+//     },
+//   };
+
+//   type Question = {
+//     title?: string;
+//     option1: {
+//       text: string,
+//       img?: string, // public 폴더 내 이미지 경로
+//     },
+//     option2: {
+//       text: string,
+//       img?: string,
+//     }
+//   }
+
+//   const handleChoice = (option: Question['option1'] | Question['option2']) => {
+//     setChoice(option.text);
+//   };
+
+
+
+//   return (
+//     <div className="game-container">
+//       <h2 className="question-title">어떤 선택을 하시겠습니까?</h2>
+//       <div className="image-options">
+        
+//         <div className="option" onClick={() => handleChoice(question.option1)}>
+//           <img src={question.option1.img} alt="Option 1" />
+//           <div className="option-text">{question.option1.text}</div>
+//         </div>
+
+//         <div className="option" onClick={() => handleChoice(question.option2)}>
+//           <img src={question.option2.img} alt="Option 2" />
+//           <div className="option-text">{question.option2.text}</div>
+//         </div>
+//       </div>
+//       {choice && <p className="result">당신의 선택: {choice}</p>}
+//     </div>
+//      );
+//     }
+    
+//     export default Game;
